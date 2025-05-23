f60e69df7e5aa43e15620c267befd8ae0ba55dae
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-23 12:13:20 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+spider.style.left = '45%';
+spider.style.top = '45%';
+spider.style.transformTranslateX = '-45%';
+spider.style.transformTranslateY = '-45%';
