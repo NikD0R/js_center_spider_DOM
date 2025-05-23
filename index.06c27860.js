b30ac5e8be5d3e3cb8d72efe86da70cd59cb9b8a
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-23 12:14:09 +0300

    make build

---
+++ index.06c27860.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider");e.style.left="45%",e.style.top="45%",e.style.transformTranslateX="-45%",e.style.transformTranslateY="-45%";
+//# sourceMappingURL=index.06c27860.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const spider = document.querySelector(".spider");
+spider.style.left = "45%";
+spider.style.top = "45%";
+spider.style.transformTranslateX = "-45%";
+spider.style.transformTranslateY = "-45%";
+
+//# sourceMappingURL=index.f75de5e1.js.map
